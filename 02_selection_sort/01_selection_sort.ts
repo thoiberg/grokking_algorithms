2144808395f0594afbb473862fdb8b5ecba96da2
Tim Hoiberg <tim.hoiberg@gmail.com>
2020-02-05 16:56:51 +1100

    Add function to implement selection sort

--- 02_selection_sort/01_selection_sort.ts
+++ 02_selection_sort/01_selection_sort.ts
@@ -12,4 +12,19 @@ function findSmallest<T>(array: Array<T>): number {
   return smallestIndex
 }
 
-console.log(findSmallest([5, 3, 6, 2, 10]))
+console.log("findSmallest", findSmallest([5, 3, 6, 2, 10])) // index pos of 2 (smallest number) is 3
+
+function selectionSort<T>(array: Array<T>): Array<T> {
+  let newArray: Array<T> = []
+
+  let arrayLength = array.length
+  for (let i = 0; i < arrayLength; i++) {
+    let smallestIndex = findSmallest(array)
+    let smallestValue = array.splice(smallestIndex, 1)[0]
+    newArray.push(smallestValue)
+  }
+
+  return newArray
+}
+
+console.log("selectionSort", selectionSort([5, 3, 6, 2, 10])) // [2, 3, 5, 6, 10]
